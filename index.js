93e289ca00be1ba4f6e803bfe388207f094de690
Ahmed Al-kheerow <a.alkheerow@gmail.com>
2020-05-30 02:15:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -58,4 +58,3 @@ const [k_name,k_color,k_album,k_job,k_partner] =k_muppet;
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
 const [song_2,song_4] = k_muppet.k_album.the_muppet_movie;
 const [k_job,k_partner] = k_muppet;
-
